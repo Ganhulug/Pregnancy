4832269d6b5663f0b6acdd8fbcce7bc3e61e4ee9
Ganhulug <ganaa4223@gmail.com>
2018-11-03 13:25:16 +0800

    Ерөнхий загвар v1
    Ерөнхий загвар v1

---
+++ app/src/main/java/com/example/xps/pregnancy/Advice_activity.java
@@ -0,0 +1,39 @@
+package com.example.xps.pregnancy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Advice_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advice);
+    }
+
+    public void openHome(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), Home_activity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void openCalendar(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), Calendar_activity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void openAdvice(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), Advice_activity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void openMore(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), More_activity.class);
+        startActivityForResult(i, 1);
+    }
+}
---
+++ app/src/main/java/com/example/xps/pregnancy/Home_activity.java
@@ -0,0 +1,41 @@
+package com.example.xps.pregnancy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activity);
+    }
+
+    public void openHome(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), Home_activity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void openCalendar(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), Calendar_activity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void openAdvice(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), Advice_activity.class);
+        startActivityForResult(i, 1);
+    }
+
+    public void openMore(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), More_activity.class);
+        startActivityForResult(i, 1);
+    }
+
+
+}
